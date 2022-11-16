e562040028e35f430a43d03dfc92275f13d7bdf6
Salo <salomonco@gmail.com>
2022-11-16 15:39:46 -0500

    hooked up api and it responds

--- weather-api/src/Weather.js
+++ weather-api/src/Weather.js
@@ -1,67 +1,68 @@
 import {useState} from 'react'
 import './Weather.css'
-
-
+import RadioButton from './RadioButton'
 
 function Weather() {
+  const [zip, setZip] = useState("33180");
+  const [unit, setUnit] = useState("metric");
+  const [data, setData] = useState(null);
 
-    const [zip, setZip] = useState('33180')
-    const [unit, setUnit] = useState('metric')
+  // -----------------------------------
+    async function fetchWeather(){
+        const apikey = "7e247499e6bbed93c3bd35baf2018838";
+        const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${unit}`;
+        const res = await fetch(path)
+        const json = await res.json()
+        console.log(json)
+    }
+  // -----------------------------------
 
-    return (
-      <div className="Weather">
-        <h1>
-          {zip} {unit}
-        </h1>
-        <form>
-          <div>
-            <input
-              placeholder="Enter Zip Code"
-              value={zip}
-              onChange={(e) => setZip(e.target.value)}
-            />
-            <button>Submit</button>
-          </div>
-          <select 
-          value={unit}
-          onChange={(e) => setUnit(e.target.value)}>
-            <option value="metric">Celcius</option>
-            <option value="imperial">Farenheit</option>
-            <option value="standard">Kelvin</option>
-          </select>
 
-          <label>
-            <input
-              type="radio"
-              name="unit"
-              checked={unit === "metric"}
-              onChange={() => setUnit("metric")}
-            />
-            metric
-          </label>
+  return (
+    <div className="Weather">
+      {data && <h1>{data.temp}</h1>}
 
-          <label>
-            <input
-              type="radio"
-              name="unit"
-              checked={unit === "imperial"}
-              onChange={() => setUnit("imperial")}
-            />
-            imperial
-          </label>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchWeather()
+        }}
+      >
+        <div>
+          <input
+            placeholder="Enter Zip Code"
+            value={zip}
+            onChange={(e) => setZip(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </div>
+        <select value={unit} onChange={(e) => setUnit(e.target.value)}>
+          <option value="metric">Celcius</option>
+          <option value="imperial">Farenheit</option>
+          <option value="standard">Kelvin</option>
+        </select>
 
-          <label>
-            <input
-              type="radio"
-              name="unit"
-              checked={unit === "standard"}
-              onChange={() => setUnit("standard")}
-            />
-            standard
-          </label>
-        </form>
-      </div>
-    );
+        <RadioButton
+          label="metric"
+          name="unit"
+          checked={unit === "metric"}
+          onChange={() => setUnit("metric")}
+        />
+        <RadioButton
+          label="standard"
+          name="unit"
+          checked={unit === "standard"}
+          onChange={() => setUnit("standard")}
+        />
+        <RadioButton
+          label="imperial"
+          name="unit"
+          checked={unit === "imperial"}
+          onChange={() => setUnit("imperial")}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Weather
