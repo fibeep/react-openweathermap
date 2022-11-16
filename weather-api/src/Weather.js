36653f08255dc265cd70d2e34f580266bb58b5e8
Salo <salomonco@gmail.com>
2022-11-16 15:53:47 -0500

    error message

--- weather-api/src/Weather.js
+++ weather-api/src/Weather.js
@@ -15,13 +15,21 @@ function Weather() {
         const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${unit}`;
         const res = await fetch(path)
         const json = await res.json()
-       console.log(json);
+        const cod = json.cod;
+        const message = json.message;
+
+        if (cod !== 200) {
+            setData({cod, message})
+            return
+        }
         const temp = json.main.temp
         const feelsLike = json.main.feels_like
         const desc = json.weather[0].description
-
+  
 
         setData({
+            cod,
+            message,
             temp,
             feelsLike,
             desc
--- weather-api/src/WeatherDisplay.js
+++ weather-api/src/WeatherDisplay.js
@@ -1,12 +1,18 @@
 
 
 function WeatherDisplay(props){
-    const {temp, feelsLike, description} = props
+    const {temp, feelsLike, desc, cod, message} = props
+    
+    if (cod !== 200) {
+        return(
+            <small>{message}</small>
+        )
+    }
     return(
 <div className = "Weather">
 <h1>{temp}</h1>
 <small>Feels Like: {feelsLike}</small>
-<p>{description}</p>
+<p>{desc}</p>
 </div>
     )
 }
